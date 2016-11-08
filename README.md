ad78f5cab7b02e49dedccac83f784d1d1e0a7a5a
Victor Vrantchan <vrancean+github@gmail.com>
2016-11-08 15:04:51 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Scan a binary file for x.509 objects such as certs, public or private keys.
+
+Usage:
+```
+./pkcsscan /path/to/binary/file
+````
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+)
+
+func main() {
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	var funcs = []parseFunc{
+		publicKey,
+		pkcs8priv,
+		pkcs1priv,
+		cert,
+	}
+	scanAll(data, funcs...)
+}
+
+// asn1Sequence is the delimiter used to parse PKCS data.
+var asn1sequence = []byte{48, 130}
+
+// parseFunc is a function which can be used to parse
+// some object from []byte.
+type parseFunc func([]byte) (val interface{}, err error)
+
+var (
+	pkcs8priv parseFunc = x509.ParsePKCS8PrivateKey
+	publicKey           = x509.ParsePKIXPublicKey
+	pkcs1priv           = wrapPKCS1
+	cert                = wrapCert
+)
+
+func wrapPKCS1(data []byte) (interface{}, error) {
+	result, err := x509.ParsePKCS1PrivateKey(data)
+	if result != nil && err != nil {
+		return result, nil
+	}
+	return nil, err
+}
+
+func wrapCert(data []byte) (interface{}, error) {
+	result, err := x509.ParseCertificate(data)
+	if result != nil && err != nil {
+		return result, nil
+	}
+	return nil, err
+}
+
+func scanAll(input []byte, funcs ...parseFunc) {
+	for _, fn := range funcs {
+		results := maybeFind(input, fn)
+		for idx, kind := range results {
+			fmt.Printf("found %s at index: %d\n", reflect.TypeOf(kind), idx)
+		}
+
+	}
+}
+
+func maybeFind(input []byte, fn parseFunc) map[int]interface{} {
+	results := make(map[int]interface{})
+	var result interface{}
+	var data []byte
+	data = input
+	var cursor = 0
+	for {
+		idx := bytes.Index(data, asn1sequence)
+		if idx == -1 {
+			break
+		}
+		cursor = idx + cursor
+		result, data = withOffset(data, idx, fn)
+		if result != nil {
+			results[cursor] = result
+		}
+	}
+	return results
+}
+
+func withOffset(data []byte, off int, fn parseFunc) (result interface{}, rest []byte) {
+	offset := data[off:]
+	var err error
+	for i := 1; i < len(offset); i++ {
+		result, err = fn(offset[:i])
+		if err == nil {
+			break
+		}
+	}
+	return result, offset[1:]
+}
